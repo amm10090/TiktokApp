001f88ccbe6c01a1a39428940a806235ff9b83af
Amo <amm10090@163.com>
2024-05-31 00:45:44 +0800

    测试

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,8 +5,8 @@ Page({
       { src: '/images/main2.jpg', title: '丁晴密封条', subtitle: '耐油耐磨缓震', date: '2023.7.1 - 2023.12.31' }
     ],
     buttons: [
-      { text: '公司介绍', icon: '/images/ticket.png' },
-      { text: '联系我们', icon: '/images/guide.png' }
+      { text: '公司介绍', icon: '/images/ticket.png', url: '/pages/activity/activity' },
+      { text: '联系我们', icon: '/images/guide.png', url: '/pages/my/my' },
     ],
     products: [
       {
@@ -34,5 +34,10 @@ Page({
     tt.switchTab({
       url: '/pages/activity/activity'  // 确保路径正确
     });
-  }
+  },
+  buttons1: function(event) {
+    const url = event.currentTarget.dataset.url;
+    tt.switchTab({
+      url: url,
+    });}
 });
